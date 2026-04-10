7cb789b2f3cccf5987807a43c1b152dcc89cb3bf
Shreya Pandey <ishapandey2801@gmail.com>
2026-04-10 13:18:43 +0000

    Added a program

---
+++ conditionalstatement/ticket_price.py
@@ -0,0 +1,26 @@
+## DETERMINE THE TICKET PRICE ACCORDING TO AGE WHETHER THE PERSON IS A STUDENT.
+
+age = int(input("Enter your age : "))
+is_student = input("Are you a student? (yes/no) : ").lower()
+
+if age < 5:
+    price = "Free"
+elif age <= 12: 
+    price = "$10"
+elif age <= 17:
+    if is_student == "yes":
+        price = "$12"
+    else:
+        price = "$15"
+elif age <= 64:
+    if is_student == "yes":
+        price ="$18"
+    else:
+        price = "$20"
+else:
+    price = "$25"
+
+print("Ticket price:", price)
+
+
+
