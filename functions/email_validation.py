962a92d4ed133d76b1586245a6a8ebd1f513e02f
Shreya Pandey <ishapandey2801@gmail.com>
2026-04-21 17:25:02 +0000

    Added new program

---
+++ functions/email_validation.py
@@ -0,0 +1,15 @@
+# validate email address
+import re
+
+def is_valid_email(email):
+    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    return re.match(pattern, email) is not None
+
+
+# User input
+email = input("Enter email: ")
+
+if is_valid_email(email):
+    print("Valid email ✅")
+else:
+    print("Invalid email ❌")
