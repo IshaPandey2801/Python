0b226d47cb11d7587a26f95a2f506c5b549cc971
Shreya Pandey <ishapandey2801@gmail.com>
2026-05-01 15:37:05 +0000

    Added new code

---
+++ loop/pattern/reverse_pyramid.py
@@ -0,0 +1,18 @@
+i = 4
+
+while i >= 1:
+    #space
+    j = 1
+    while j <= 4-i:
+        print(" ", end = "")
+        j += 1
+    
+    #stars
+    k = 1
+    while k <= i:
+        print("*", end=" ")
+        k += 1
+    print()
+    i -= 1
+
+
