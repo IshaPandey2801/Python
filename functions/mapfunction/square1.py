15e036c108214be015b5d8cd6d4630b3bc5bff79
Shreya Pandey <ishapandey2801@gmail.com>
2026-05-04 06:25:34 +0000

    Added new program

--- functions/mapfunction/square1.py
+++ functions/mapfunction/square1.py
@@ -2,3 +2,11 @@
 numbers = [1, 2, 3, 4, 5]
 
 print(list(map(lambda x: x*x, numbers)))
+
+
+# by taking input from users
+numbers = input("Enter the numbers: "). split()
+
+numbers= list(map(int, numbers))
+
+print(list(map(lambda x: x*x, numbers)))
